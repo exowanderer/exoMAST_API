6477acc91e9f22e9894731df3f90dcccc1f5cd28
Michael Glenn <mbg198618@gmail.com>
2021-07-09 11:42:31 -0500

    Update identifiers.js

--- identifiers.js
+++ identifiers.js
@@ -1,7 +1,5 @@
-bash wget -c -O hd187733b_identifiers.json
-''
-  -<
-  'https://exo.mast.stsci.edu/api/v0.1/exoplanets/identifiers/?name=HD189733b`>
+{
+  <bash wget -c -O hd187733b_identifiers.json'https://exo.mast.stsci.edu/api/v0.1/exoplanets/identifiers/?name=HD189733b`>
 {
   "canonicalName":
   "HD 189733 b",
